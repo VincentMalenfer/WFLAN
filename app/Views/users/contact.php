2bcfd5d910676f073132b82c798eeda8c4bebfad
oxsparks <paris-romain@alraedytomorrow.fr>
2017-02-03 11:27:17 +0100

    fin des forms
    formulaires fini et correction des direction de la navbar

--- app/Views/users/contact.php
+++ app/Views/users/contact.php
@@ -4,27 +4,34 @@
 	<h2>Contact</h2>
 	<p>Vous avez atteint la page de contact.</p>
 	<div class="container">
-		<form class="row" action="index.html" method="post">
-			<div class="form-group">
-				<input type="text" name="lastname" placeholder="votre nom">
-			</div>
-			<div class="form-group">
-				<input type="text" name="firstname" placeholder="votre prénom">
-			</div>
-			<div class="form-group">
-				<input type="text" name="nickname" placeholder="votre pseudo">
-			</div>
-			<div class="form-group">
-				<input type="email" name="email" placeholder="votre adresse email">
-			</div>
-			<div class="form-group">
-				<textarea name="contactMessage" placeholder="Votre message... "rows="8" cols="80"></textarea>
-			</div>
-			<div class="form-group">
-				<input type="submit">
-			</div>
-		</form>
+		<fieldset class="row">
+			<form class="col-lg-12 center-block form-horizontal" action="index.html" method="post">
+				<div class="form-group">
+					<label for="lastname">Votre nom</label>
+					<input type="text" name="lastname" placeholder="votre nom">
+				</div>
+				<div class="form-group">
+					<label for="firstname">Votre prénom</label>
+					<input type="text" name="firstname" placeholder="votre prénom">
+				</div>
+				<div class="form-group">
+					<label for="nickname">Votre pseudo</label>
+					<input type="text" name="nickname" placeholder="votre pseudo">
+				</div>
+				<div class="form-group">
+					<label for="email">Votre email</label>
+					<input type="email" name="email" placeholder="votre adresse email">
+				</div>
+				<div class="form-group">
+					<textarea name="contactMessage" placeholder="Votre message... "rows="8" cols="80"></textarea>
+				</div>
+				<div class="form-group">
+					<input type="submit">
+				</div>
+			</form>
+		</fieldset>
 	</div>
+
 
 
 <?php $this->stop('main_content') ?>
--- app/Views/users/sign_up.php
+++ app/Views/users/sign_up.php
@@ -7,39 +7,40 @@
 		<form class="row" action="index.html" method="post">
 			<div class="form-group">
 				<label for="lastname">Votre nom </label>
-				<input type="text" name="lastname">
+				<input type="text" name="lastname" required>
 			</div>
 			<div class="form-group">
 				<label for="firstname">Votre prénom </label>
-				<input type="text" name="firstname">
+				<input type="text" name="firstname" required>
 			</div>
 			<div class="form-group">
 				<label for="nickname">Votre pseudo </label>
-				<input type="text" name="nickname" >
+				<input type="text" name="nickname">
 			</div>
 			<div class="form-group">
 				<label for="email">Votre adresse email </label>
-				<input type="email" name="email" >
+				<input type="email" name="email" required>
 			</div>
 			<div class="form-group">
 				<label for="phone">Votre numéro de téléphone </label>
 				<input type="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" name="phone">
-				<small class="form-text text-muted">* n'est pas obligatoire</small>
+				<small class="form-text text-muted"><br>* n'est pas obligatoire</small>
 			</div>
 			<div class="form-group">
 				<label for="birthdate">Votre date de naissance</label>
-				<input type="date" name="birthdate">
+				<input type="date" name="birthdate" required>
 			</div>
 			<div class="form-group">
 				<label for="password">Votre mot de passe </label>
-				<input type="password" name="password" >
+				<input type="password" name="password" required>
 			</div>
 			<div class="form-group">
 				<label for="passwordConfirm">Confirmation de votre mot de passe </label>
-				<input type="passwordConfirm" name="passwordConfirm" >
+				<input type="passwordConfirm" name="passwordConfirm" required>
 			</div>
 			<div class="form-group">
 				<textarea name="contactMessage" placeholder="Votre message..." style="width:auto"></textarea>
+				<small class="form-text text-muted"><br>* les informations entrée ne seront pas partagée ou utilisée à des fin commecricale.</small>
 			</div>
 			<div class="form-group">
 				<input type="submit">
