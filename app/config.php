a2827bb583c8ce816df3d322d1ea746e79e6bb58
VincentMalenfer <vincent.malenfer@outlook.fr>
2017-02-10 10:17:11 +0100

    MAJ du 10/02

--- app/config.php
+++ app/config.php
@@ -8,20 +8,18 @@ $w_config = [
   'db_name' 							=> 'wflanfreorwfan',			//nom de la bdd
   'db_table_prefix' 			=> '',										//préfixe ajouté aux noms de table
 
-
 		//authentification, autorisation
 	'security_user_table' 					=> 'users',				//nom de la table contenant les infos des utilisateurs
 	'security_id_property' 					=> 'id',					//nom de la colonne pour la clef primaire
 	'security_username_property' 		=> 'username',		//nom de la colonne pour le "pseudo"
 	'security_email_property' 			=> 'email',				//nom de la colonne pour l'"email"
 	'security_password_property' 		=> 'password',		//nom de la colonne pour le "mot de passe"
-<<<<<<< HEAD
-	'security_role_property' 			=> 'role',				//nom de la colonne pour le "role"
-	'users_sign_in' 							=> 'login',				//nom de la route affichant le formulaire de connexion
-=======
-	'security_role_property' 			=> 'role',				//nom de la colonne pour le "role"
+	'security_role_property' 				=> 'role',				//nom de la colonne pour le "role"
+	'users_sign_in' 								=> 'login',				//nom de la route affichant le formulaire de connexion
 
-	'security_login_route_name' 	=> 'login',				//nom de la route affichant le formulaire de connexion
+	'security_role_property' 				=> 'role',				//nom de la colonne pour le "role"
+
+	'security_login_route_name' 		=> 'login',				//nom de la route affichant le formulaire de connexion
 
 
 		// configuration globale
