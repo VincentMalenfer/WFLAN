90cb5b13ef812593d34e831e6e6eb84a08193e2e
thibaudmolitor <thibaud.molitor@gmail.com>
2017-02-11 17:08:50 +0100

    maj function add article

--- app/Controller/ArticlesController.php
+++ app/Controller/ArticlesController.php
@@ -7,15 +7,7 @@ use \Model\ArticlesModel;
 
 class ArticlesController extends Controller
 {
-    // Affichage côté administrateur
-    public function article()
-    {
-        // $this->allowTo('admin');
-
-        $articleModel = new ArticlesModel();
-        $games = $articleModel->getGame();
-        $this->show('admin/admin', ['games'=> $games]);
-    }
+    
 
     // Affichage d'un article côté utilisateur
     function showArticles($id)
@@ -25,13 +17,7 @@ class ArticlesController extends Controller
       $this->show('articles/article', ['article'=> $articles]);
     }
 
-    // Affichage de la liste des articles côté administrateur
-    public function admin_list_articles()
-	{
-        $articleModel = new ArticlesModel();
-        $articles = $articleModel->getArticles();
-        $this->show('admin/admin_list_articles', ['articles'=> $articles]);
-	}
+  
 
     // Liste de tout les articles côté utilisateur
     function listArticles()
@@ -40,13 +26,19 @@ class ArticlesController extends Controller
       $articles = $articleModel->getArticles();
       $this->show('articles/list_articles', ['articles'=> $articles]);
     }
-
+      // Affichage de la liste des articles côté administrateur
+    public function admin_list_articles()
+    {
+        $articleModel = new ArticlesModel();
+        $articles = $articleModel->getArticles();
+        $this->show('admin/admin_list_articles', ['articles'=> $articles]);
+    }
     // Supprime un article
     public function suppArticle($id)
     {
         $supp= new ArticlesModel();
         $supp->deleteArticle($id);
-        $this->redirectToRoute('list_articles');
+        $this->redirectToRoute('admin/admin_list_articles');
     }
 
     // Modifie un article
@@ -59,10 +51,9 @@ class ArticlesController extends Controller
 
     // Permet d'ajouter un article
     public function addArticle(){
-
         // Liste de games
-            $articleModel = new ArticlesModel();
-            $games = $articleModel->getGame();
+        $articleModel = new ArticlesModel();
+        $games = $articleModel->getGame();
 
         // $this->allowTo('admin');
         $filepath="";
@@ -72,10 +63,9 @@ class ArticlesController extends Controller
 
             // revoir le chemin de destination des images
 
-            $dir = '../../public/assets/img/articles/';
+            $dir =  'assets/img/articles/';
 
-            // je verifie que le dossier de destination existe
-
+            // je verifie que le dossier de destination existe   
             if (file_exists($dir)&& is_dir($dir)) {
 
                 // $filename definit le nom définitif de l'image et on lui colle son extension (pdf...) d'où le "."
@@ -85,13 +75,13 @@ class ArticlesController extends Controller
                 // je deplace le fichier depuis le dossier temporaire vers la destination
 
                 if (move_uploaded_file($_FILES['picture']['tmp_name'],$dir.$filename)) {
-                    $filepath =	'../../public/assets/img/articles/'.$filename;
+                    $filepath = 'assets/img/articles/'.$filename;
                 }else{
                     die("upload failed");
                 }
             }
         }
-        if(!empty($_POST) && $this->security()==true){
+        if(!empty($_POST) && htmlentities($_POST['title'])<50 && htmlentities($_POST['description'])<30 && htmlentities($_POST['description_pictures'])){
             $addArticle = new ArticlesModel();
             $last_id = $addArticle->addArcticle(
                                     $_POST['title'],
@@ -100,20 +90,25 @@ class ArticlesController extends Controller
                                     $filepath,
                                     $_POST['description_pictures']
                                     );
-
+            
+        
         };
 
-        ///////////////////////////////////////////////////////////////
-
-        $this->show('list_articles');
+        if(!empty($_POST['checkbox'])){
+            $idGame     = $_POST['checkbox'];
+            
+            $id_article = $last_id['idarticles'];
+            $checkbox   = new ArticlesModel();
+            $checkbox->articleHaveGame($idGame,$id_article);   
+        }
     }
+        ///////////////////////////////////////////////////////////////
+        //
 
-    // Vérifie les champs côté administrateur
-    public function security(){
-		if (htmlentities($_POST['title'])<50 && htmlentities($_POST['description'])<30 && htmlentities($_POST['description_pictures']) ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+        // $this->show('list_articles');
+    
+
+    
+   
+		
 }
--- app/Model/ArticlesModel.php
+++ app/Model/ArticlesModel.php
@@ -26,6 +26,9 @@ class ArticlesModel extends Model
 	public function addArcticle($title,$description,$text,$pictures,$description_pictures)
 	{
 		$this->setPrimaryKey("idarticles");
+		$iduser= new GeneralModel;
+		$idusertoken= $iduser->getIdFromToken($_SESSION["token"]);
+		$iduserinsert =implode($idusertoken); 
 		$data= array(
 			'title' 					=> $title,				//post titre du form admin article
 			'description' 				=> $description,				//post description du form de admin article
@@ -34,21 +37,29 @@ class ArticlesModel extends Model
 			'description_pictures'		=> $description_pictures,			//post des_pictures du form admin article
 			'publishdate'				=> date("Y-m-d"),				// = date du serveur
 			'author'					=> $_SESSION["nickname"],
-			"users_idusers" => getIdFromToken($_SESSION["token"])
+			"users_idusers" 			=> 	$iduserinsert
+
 
 			);
-		return $this->insert($data);
+			
+			return $this->insert($data);
 		// 'INSERT INTO articles (title,description,`text`,pictures,des_pictures,publishdate,author )
 		// VALUES (:title,:description;`:text`,:pictures,:picturesDes,:publishdate,:autor)';
 	}
 
-	public function articleHaveGame($idGame,$id_article){
-		unset($data);
+	public function articleHaveGame($game,$id_article){
+		
+		$this->setPrimaryKey("games_idgames");
+		$this->setTable('games_has_articles');
+
 		$data= array(
-			'idgames'		=>  $idGame,
-			'idarticles'		=>  $id_article
+			'games_idgames'			=>  $game,
+			'articles_idarticles'	=>  $id_article  
 			);
-		return $this->insert($data);
+		
+		$sql = 'INSERT INTO games_has_articles (`articles_idarticles`, `games_idgames`) VALUES (:articles_idarticles,:games_idgames)'; 
+		$pouet = $this->dbh->prepare($sql);
+    	$pouet->execute($data);
 	}
 
 	public function deleteArticle($actif)
