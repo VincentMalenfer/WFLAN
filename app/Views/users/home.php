82418c7c535f111aa35959d8e2af83c5150233b8
oxsparks <paris-romain@alraedytomorrow.fr>
2017-02-13 11:07:55 +0100

    worming slider

--- app/Views/users/home.php
+++ app/Views/users/home.php
@@ -1,43 +1,45 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
+
 	<!-- Carousel -->
 	<div class="carousel slide" data-ride="carousel" id="carousel-1">
-		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<?php foreach ($articles as $article):?>
-					<div class="jumbotron hero-nature carousel-hero">
-						<div class="fill" style="background-image:url('<?= $article['pictures'] ?>');"></div>
-						<h2 class="hero-title"><?= $article['title'] ?></h2>
-						<p class="hero-subtitle"><?= $article['description']."..." ?></p>
-						<p><a class="btn btn-primary btn-lg hero-button" role="button" href="<?=$this->url('article_article', ['id' => $article['idarticles']])?>">Lire l'article</a></p>
-					</div>
-			</div>
+	  <div class="carousel-inner" role="listbox">
+			<?php
+			$i = 0;
+			foreach ($articles as $article):
+			?>
+	    <div class="item <?php if ($i == 0) { echo 'active'; } ?>">
+	        <div class="jumbotron hero-nature carousel-hero">
+					<style>
+						.jumbotron.carousel-hero{
+							background: url('<?= $article['pictures'] ?>');
+						}
+					</style>
+	          <h2 class="hero-title"><?= $article['title'] ?></h2>
+	          <p class="hero-subtitle"><?= $article['description']."..." ?></p>
+	          <p><a class="btn btn-primary btn-lg hero-button" role="button" href="<?=$this->url('article_article', ['id' => $article['idarticles']])?>">Lire l'article</a></p>
+	        </div>
+	    </div>
+			<?php
+			$i++;
+			endforeach;
+			?>
 
-			 <div class="item">
-				<div class="jumbotron hero-photography carousel-hero">
-					<h2 class="hero-title">Hero Photography</h2>
-					<p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
-					<p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Learn more</a></p>
-				</div>
-			</div>
-			<div class="item">
-				<div class="jumbotron hero-technology carousel-hero">
-					<h2 class="hero-title">Hero Technology</h2>
-					<p class="hero-subtitle">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.</p>
-					<p><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Learn more</a></p>
-				</div>
-			</div>
-		</div>
-<?php endforeach;?>
-		<div><a class="left carousel-control" href="#carousel-1" role="button" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i><span class="sr-only">Previous</span></a>
-				 <a class="right carousel-control" href="#carousel-1" role="button" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i><span class="sr-only">Next</span></a>
-		</div>
-		<ol class="carousel-indicators">
-			<li data-target="#carousel-1" data-slide-to="0" class="active"></li>
-			<li data-target="#carousel-1" data-slide-to="1"></li>
-			<li data-target="#carousel-1" data-slide-to="2"></li>
-		</ol>
+	  <div><a class="left carousel-control" href="#carousel-1" role="button" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i><span class="sr-only">Previous</span></a>
+	       <a class="right carousel-control" href="#carousel-1" role="button" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i><span class="sr-only">Next</span></a>
+	  </div>
+	  <ol class="carousel-indicators">
+			<?php
+			$i = 0;
+			foreach ($articles as $article):
+			?>
+	    <li data-target="#carousel-1" data-slide-to="<?= $i ?>" class="<?php if ($i == 0) { echo 'active'; } ?>"></li>
+			<?php
+			$i++;
+			endforeach;
+			?>
+	  </ol>
 	</div>
 
 	<!-- CountDown prochain event -->
